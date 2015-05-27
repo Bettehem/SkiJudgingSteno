20e30c693db421ff1079f38a15d736e97c854d97
FlyingNugget <aleksi.hakala.98@gmail.com>
2015-05-27 18:13:28 +0300

    es

--- app/src/main/java/com/bettehem/skijudgingsteno/SkiSlopestyleEvent.java
+++ app/src/main/java/com/bettehem/skijudgingsteno/SkiSlopestyleEvent.java
@@ -23,7 +23,6 @@ public class SkiSlopestyleEvent extends ActionBarActivity implements View.OnClic
     Spinner skiSlopestyleEventSelectExistingEventSpinner;
     SavingAndLoadingEvents savingAndLoadingEvents;
     TextView skiSlopestyleExistingEventTextView;
-    //ES
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
