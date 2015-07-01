d7ffab2ac2fb0971178721ad1cdb213df7685cb5
Bettehem <chris.mustola@gmail.com>
2015-07-01 03:35:52 +0300

    Added a new "feature". When a user resets the app, a progress bar will open, and the actual deletion happens in a separate thread, so the app will not freeze if the user has a lot to delete.

--- app/src/main/java/com/bettehem/skijudgingsteno/ResetApp.java
+++ app/src/main/java/com/bettehem/skijudgingsteno/ResetApp.java
@@ -1,6 +1,8 @@
 package com.bettehem.skijudgingsteno;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.os.AsyncTask;
 
 /*
  Copyright 2015 Chris Mustola
@@ -21,49 +23,127 @@ public class ResetApp {
     private SavingAndLoadingProfiles savingAndLoadingProfiles = new SavingAndLoadingProfiles();
     private SavingAndLoadingEvents savingAndLoadingEvents = new SavingAndLoadingEvents();
 
+    private int profileAmount, profileFileAmount, eventAmount, eventFileAmount, settingsFileAmount, totalFilesToDelete;
+    private ProgressDialog progressDialog;
+
     public void resetAppData(Context context){
-		
-		savingAndLoading.preferenceFilename = savingAndLoadingProfiles.originalProfileDetailsFileName;
-		if (savingAndLoading.checkIfFileExists(context)){
-        	profileDeletion(context);
-		}
-		
-		savingAndLoading.preferenceFilename = savingAndLoadingEvents.originalEventDetailsFilename;
-		if (savingAndLoading.checkIfFileExists(context)){
-			eventDeletion(context);
-		}
-		
-		savingAndLoading.preferenceFilename = savingAndLoading.originalPreferenceFilename;
-		if (savingAndLoading.checkIfFileExists(context)){
-			settingsDeletion(context);
-		}
-			
+
+        calculateData(context);
+
+        showProgressDialog(context);
+
+        new DeleteData().execute(context);
     }
 
-    private void profileDeletion(Context context){
+    private void calculateData(Context context){
         savingAndLoading.preferenceFilename = savingAndLoadingProfiles.originalProfileDetailsFileName;
-        while (!savingAndLoading.loadString(context, savingAndLoadingProfiles.profileListName).contentEquals("")){
-            String[] profileList = savingAndLoading.loadStringArray(context, savingAndLoadingProfiles.profileListName);
-            savingAndLoadingProfiles.deleteProfile(context, profileList[profileList.length-1]);
+        if (savingAndLoading.loadString(context, savingAndLoadingProfiles.profileListName).contentEquals("")){
+            profileAmount = 0;
+        }else{
+            profileAmount = savingAndLoading.loadStringArray(context, savingAndLoadingProfiles.profileListName).length;
         }
-
-        savingAndLoading.preferenceFilename = savingAndLoadingProfiles.originalProfileDetailsFileName;
-        savingAndLoading.deleteAllValues(context);
-    }
-
-    private void eventDeletion(Context context){
-        savingAndLoading.preferenceFilename = savingAndLoadingEvents.originalEventDetailsFilename;
-        while (!savingAndLoading.loadString(context, savingAndLoadingEvents.eventListName).contentEquals("")){
-            String[] eventList = savingAndLoading.loadStringArray(context, savingAndLoadingEvents.eventListName);
-            savingAndLoadingEvents.deleteEvent(context, eventList[eventList.length-1]);
+        if (savingAndLoading.checkIfFileExists(context)){
+            profileFileAmount = 1;
         }
 
         savingAndLoading.preferenceFilename = savingAndLoadingEvents.originalEventDetailsFilename;
-        savingAndLoading.deleteAllValues(context);
-    }
+        if (savingAndLoading.loadString(context, savingAndLoadingEvents.eventListName).contentEquals("")){
+            eventAmount = 0;
+        }else{
+            eventAmount = savingAndLoading.loadStringArray(context, savingAndLoadingEvents.eventListName).length;
+        }
+        if (savingAndLoading.checkIfFileExists(context)){
+            eventFileAmount = 1;
+        }
 
-    private void settingsDeletion(Context context){
         savingAndLoading.preferenceFilename = savingAndLoading.originalPreferenceFilename;
-        savingAndLoading.deleteAllValues(context);
+        if (savingAndLoading.checkIfFileExists(context)){
+            settingsFileAmount = 1;
+        }
+
+        totalFilesToDelete = profileAmount + profileFileAmount + eventAmount + eventFileAmount + settingsFileAmount;
     }
+
+    private void showProgressDialog(Context context){
+        progressDialog = new ProgressDialog(context);
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        progressDialog.setTitle("Please wait...");
+        progressDialog.setMessage("Resetting app data...");
+        progressDialog.setProgress(0);
+        progressDialog.setMax(totalFilesToDelete);
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+    }
+
+    private class DeleteData extends AsyncTask<Context, Integer, Boolean>{
+        @Override
+        protected Boolean doInBackground(Context... params) {
+
+            savingAndLoading.preferenceFilename = savingAndLoadingProfiles.originalProfileDetailsFileName;
+            if (savingAndLoading.checkIfFileExists(params[0])){
+                profileDeletion(params[0]);
+            }
+
+            savingAndLoading.preferenceFilename = savingAndLoadingEvents.originalEventDetailsFilename;
+            if (savingAndLoading.checkIfFileExists(params[0])){
+                eventDeletion(params[0]);
+            }
+
+            savingAndLoading.preferenceFilename = savingAndLoading.originalPreferenceFilename;
+            if (savingAndLoading.checkIfFileExists(params[0])){
+                settingsDeletion(params[0]);
+            }
+
+            return null;
+        }
+
+        private void profileDeletion(Context context){
+            savingAndLoading.preferenceFilename = savingAndLoadingProfiles.originalProfileDetailsFileName;
+            while (!savingAndLoading.loadString(context, savingAndLoadingProfiles.profileListName).contentEquals("")){
+                String[] profileList = savingAndLoading.loadStringArray(context, savingAndLoadingProfiles.profileListName);
+                savingAndLoadingProfiles.deleteProfile(context, profileList[profileList.length-1]);
+
+                publishProgress(1);
+
+            }
+
+            savingAndLoading.preferenceFilename = savingAndLoadingProfiles.originalProfileDetailsFileName;
+            savingAndLoading.deleteAllValues(context);
+
+            publishProgress(1);
+
+        }
+
+        private void eventDeletion(Context context){
+            savingAndLoading.preferenceFilename = savingAndLoadingEvents.originalEventDetailsFilename;
+            while (!savingAndLoading.loadString(context, savingAndLoadingEvents.eventListName).contentEquals("")){
+                String[] eventList = savingAndLoading.loadStringArray(context, savingAndLoadingEvents.eventListName);
+                savingAndLoadingEvents.deleteEvent(context, eventList[eventList.length-1]);
+                publishProgress(1);
+            }
+
+            savingAndLoading.preferenceFilename = savingAndLoadingEvents.originalEventDetailsFilename;
+            savingAndLoading.deleteAllValues(context);
+            publishProgress(1);
+        }
+
+        private void settingsDeletion(Context context){
+            savingAndLoading.preferenceFilename = savingAndLoading.originalPreferenceFilename;
+            savingAndLoading.deleteAllValues(context);
+
+            publishProgress(1);
+
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            progressDialog.incrementProgressBy(values[0]);
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            progressDialog.dismiss();
+        }
+    }
+
 }
